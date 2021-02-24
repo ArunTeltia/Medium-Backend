b705a84ac1f0f96a720e82f267d98023e5891412
ArunTeltia <teltia.arun18@gmail.com>
2021-02-24 22:02:54 +0530

    🙌 Added some virtual properties in story model 🙌

--- models/story.js
+++ models/story.js
@@ -3,16 +3,40 @@ const mongoose = require("mongoose");
 const storySchema = new mongoose.Schema({
   title: String,
   body: String,
-  clapCount: {
-    type: Number,
-    default: 0,
-  },
-  author: { type: mongoose.SchemaTypes.ObjectId, ref: "User" },
-  parent: { type: mongoose.SchemaTypes.ObjectId, ref: "Story" },
-  replies: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Story" }],
-  claps: [{ type: mongoose.SchemaTypes.ObjectId, ref: "Clap" }],
+  author: { type: mongoose.SchemaTypes.ObjectId, ref: "users" },
+  parent: { type: mongoose.SchemaTypes.ObjectId, ref: "stories" },
 });
 
-const Story = mongoose.model("Story", storySchema);
+storySchema.virtual("claps", {
+  ref: "claps",
+  localField: "_id",
+  foreignField: "story",
+});
+
+storySchema.virtual("clapsCount", {
+  ref: "claps",
+  localField: "_id",
+  foreignField: "story",
+  count: true,
+});
+
+storySchema.virtual("replies", {
+  ref: "stories",
+  localField: "_id",
+  foreignField: "parent",
+});
+
+storySchema.virtual("repliesCount", {
+  ref: "stories",
+  localField: "_id",
+  foreignField: "replies",
+  count: true,
+});
+
+const Story = mongoose.model("stories", storySchema);
 
 module.exports = Story;
+
+//let story = await Story.create({title: "How to use Node like Pro",....})
+// extract claps from story directly by story.clapsCoint 
+// replies ---> story.replies
